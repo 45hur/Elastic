8dfb9c4e81274b609b6a03577fb3e7d60a324893
Unknown <mikulasek@live.com>
2017-08-07 19:29:28 +0200

    Initial commit

---
+++ WB.Alerting.Connector/WB.Alerting.Connector/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//using ElasticSearch.Client.ElasticSearch;
+
+namespace WB.Alerting.Connector
+{
+    public class Client
+    {
+        public static void ElasticClient()
+        {
+            //var client = new ElasticsearchClient();
+        }
+    }
+}
